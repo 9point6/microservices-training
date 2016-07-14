5165d803680d40fd0b810ecafb64633c247b2fb0
dionysiaBBC <dionysia.agathocleous@bbc.co.uk>
2016-07-14 16:06:48 +0100

    creating tests

---
+++ cuke/features/NitroGateway.feature
@@ -0,0 +1,14 @@
+Feature: NitroGateway Feature
+AS A User
+I WANT to add in one place my favourite episodes, clips and programmes
+SO THAT I can revisit them
+
+@test
+Scenario: provide asset for specific pid
+  Given I get an asset with pid abc
+  When the pid exists
+  Then the response of the pid has the following attributes
+  |pid|
+  |title|
+  |image|
+  |type|
---
+++ cuke/features/step_definitions/nitro_gateway_steps.rb
@@ -0,0 +1,12 @@
+Given(/^I get an asset with pid (.*)$/) do |arg|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the pid exists$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the response of the pid has the following attributes$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
