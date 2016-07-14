a364f6fba616aa6ee017a5f6a2f9b1669395a052
John Sanderson <john.sanderson@bbc.co.uk>
2016-07-14 11:14:42 +0100

    Switch to multiple processes for nitro

---
+++ bin/nitro-gateway.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+// Gets list for given user
+app.get('/get-asset/:pid', require('../lib/routes/get-nitro-asset'));
+
+app.listen(3001, function () {
+    console.log('Nitro Gateway listening on port 3001');
+});
